6ff4812fe1ceeeb11b95df6479c7a1b0d3464a96
Brenda De La O Florindez <brenda.delao.florindez@vallegrande.edu.pe>
2024-11-14 16:12:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//Importar la libreria  
+const express = require('express');  
+
+//Agrega  
+const PORT = 3000;  
+
+//Objetos para llamar los metodos de express  
+const app = express();  
+
+//Ruta de archivos estáticos  
+app.use(express.static("public"));  
+
+// escucha conexiones en el puerto 3000 y muestra por consola la direccion web.  
+app.listen(PORT, () => console.log(`http://54.91.34.241:${PORT}`));
